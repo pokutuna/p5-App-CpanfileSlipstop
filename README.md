754b6bef9895352f4d312177d29e1f8126593d83
pokutuna <popopopopokutuna@gmail.com>
2018-08-04 00:17:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::CpanfileSlipstop - It's new $module
+
+# SYNOPSIS
+
+    use App::CpanfileSlipstop;
+
+# DESCRIPTION
+
+App::CpanfileSlipstop is ...
+
+# LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+pokutuna <popopopopokutuna@gmail.com>
---
+++ lib/App/CpanfileSlipstop.pm
@@ -0,0 +1,39 @@
+package App::CpanfileSlipstop;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::CpanfileSlipstop - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::CpanfileSlipstop;
+
+=head1 DESCRIPTION
+
+App::CpanfileSlipstop is ...
+
+=head1 LICENSE
+
+Copyright (C) pokutuna.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+pokutuna E<lt>popopopopokutuna@gmail.comE<gt>
+
+=cut
+
